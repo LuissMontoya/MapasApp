f039bceaee5c230bda500fc29b54005cf2210c9c
unknown <luis.montoya@udla.edu.co>
2021-09-24 18:58:58 -0500

    zoomLevel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mapasApp';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+  }
+
 }
--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,5 @@
-import { environment } from './../../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -19,11 +18,14 @@ export class FullScreenComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.mapboxToken;
+
+
     ('pk.eyJ1IjoibHVpc3Ntb250b3lhcCIsImEiOiJja3R1bnJ6MWQyMWtoMzJvOW11OGdza2FyIn0.Q1lBlP3r1C1GPLKJKmwv5g');
     var map = new mapboxgl.Map({
       container: 'mapa',
       style: 'mapbox://styles/mapbox/streets-v11',
+      center:[ -76.04576435744507,1.9919643233842774],
+      zoom:16
     });
   }
 }
