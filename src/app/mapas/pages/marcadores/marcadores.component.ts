b4d274b77d8bd8aad0eddb553fd47349629bb077
unknown <luis.montoya@udla.edu.co>
2021-09-25 23:14:01 -0500

    marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorColor {
+  color: string;
+  marker: mapboxgl.Marker;
+}
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+
+      .list-group {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        z-index: 99;
+      }
+
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-76.04576435744507, 1.9919643233842774];
 
-  constructor() { }
+  //Arreglo de marcadores
+  marcadores: MarcadorColor[] = [];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    //const markerHtml : HTMLElement = document.createElement('div');
+    //markerHtml.innerHTML = 'Hola Mundo';
+
+    //Añadir el marcador
+    //new mapboxgl.Marker()
+    //.setLngLat(this.center)
+    //.addTo(this.mapa);
   }
 
+  AgregarMarcador() {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const newMarker = new mapboxgl.Marker({
+      draggable: true,
+      color,
+    })
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+
+    this.marcadores.push({
+      color,
+      marker: newMarker });
+  }
+
+  irMarcador(marker: mapboxgl.Marker) {
+    this.mapa.flyTo({
+      center: marker.getLngLat()
+    });
+  }
+
+  guardarMarcadoresLocalStorage(){
+
+  }
+
+  leerLocalStorage(){
+
+  }
 }
